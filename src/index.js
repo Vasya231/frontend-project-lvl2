5f71431bd767433d3b2fd1dd3a1624fc3b2ff812
Vasya231 <gg231589@gmail.com>
2020-04-01 14:51:56 +0300

    add -f option, change usage description

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import program from 'commander';
 
 const main = () => {
   program.version('0.0.1')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .usage('[options] <firstConfig> <secondConfig>')
+    .option('-f, --format [type]', 'output format');
 
   program.parse(process.argv);
 };

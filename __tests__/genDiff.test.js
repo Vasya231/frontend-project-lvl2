d182f302da1fc224baa30c53f35b193c9246daa2
Vasya231 <gg231589@gmail.com>
2020-04-02 12:00:20 +0300

    add second fixture for flat test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,14 +4,19 @@ const before1 = `${__dirname}/fixtures/before1.json`;
 const after1 = `${__dirname}/fixtures/after1.json`;
 const expected1 = [
   '  host: hexlet.io',
-  '- timeout: 50',
-  '+ timeout: 20',
   '- proxy: 123.234.53.22',
   '- follow: false',
   '+ verbose: true',
+  /- timeout: 50\n\+ timeout: 20|\+ timeout: 50\n- timeout: 20/,
 ];
+const before2 = `${__dirname}/fixtures/before2.json`;
+const after2 = `${__dirname}/fixtures/after2.json`;
+const expected2 = '+ host: hexlet.io';
 
-test('simple', () => {
+test('one level', () => {
   const diff1 = genDiff(before1, after1);
   expected1.forEach((str) => expect(diff1).toMatch(str));
+
+  const diff2 = genDiff(before2, after2);
+  expect(diff2).toMatch(expected2);
 });

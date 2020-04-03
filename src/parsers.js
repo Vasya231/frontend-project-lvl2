e26aa9088dafd4ea152cff1070e345aa634e72f0
Vasya231 <gg231589@gmail.com>
2020-04-03 11:49:53 +0300

    fix style in parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -7,7 +7,7 @@ const parsers = {
   yaml: yaml.safeLoad,
   ini: (data) => {
     const dataObj = ini.parse(data);
-    const normalizedPairs = _.toPairs(dataObj).map(([key, value]) =>[key, Number(value) || value]);
+    const normalizedPairs = _.toPairs(dataObj).map(([key, value]) => [key, Number(value) || value]);
     return _.fromPairs(normalizedPairs);
   },
 };

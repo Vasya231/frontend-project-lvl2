8497d69b777015c3138c7cb0540624b496daf7f5
Vasya231 <gg231589@gmail.com>
2020-04-05 13:40:34 +0300

    change parsers realization

--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,10 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
-import _ from 'lodash';
 
 const parsers = {
   json: JSON.parse,
   yaml: yaml.safeLoad,
-  ini: (data) => {
-    const dataObj = ini.parse(data);
-    const normalizedPairs = _.toPairs(dataObj).map(([key, value]) => [key, Number(value) || value]);
-    return _.fromPairs(normalizedPairs);
-  },
+  ini: ini.parse,
 };
 
-export default (format) => ({ parse: parsers[format] });
+export default (format, data) => (parsers[format](data));

95c73cc48181813d798c7399fdabe9878219a479
Vasya231 <gg231589@gmail.com>
2020-04-07 19:35:26 +0300

    refactor: remove old diffTree realization artefact

--- src/nodes.js
+++ src/nodes.js
@@ -6,23 +6,6 @@ const createSimpleNode = (name, type, value) => ({
   value,
 });
 
-const createSingleSourceNode = (name, type, value) => {
-  if (!_.isPlainObject(value)) {
-    return createSimpleNode(name, type, value);
-  }
-  const entries = Object.entries(value);
-  const children = entries.reduce(
-    (acc, [key, val]) => [...acc, (_.isPlainObject(val) ? createSingleSourceNode(key, 'regular', val)
-      : createSimpleNode(key, 'regular', val))],
-    [],
-  );
-  return {
-    name,
-    type,
-    children,
-  };
-};
-
 const createDiffNode = (name, valueBefore, valueAfter) => ({
   name,
   type: 'diff',
@@ -42,7 +25,7 @@ const createDualSourceNode = (name, objBefore, objAfter) => {
         return [...acc, createDualSourceNode(key, valueBefore, valueAfter)];
       }
       if (valueBefore === valueAfter) {
-        return [...acc, createSingleSourceNode(key, 'regular', valueBefore)];
+        return [...acc, createSimpleNode(key, 'regular', valueBefore)];
       }
       return [...acc, createDiffNode(key, valueBefore, valueAfter)];
     },

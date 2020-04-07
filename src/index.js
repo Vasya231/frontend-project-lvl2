4ac7ae3ef120bf319f1d86fe9cf37c6e2fb084cd
Vasya231 <gg231589@gmail.com>
2020-04-07 13:39:35 +0300

    feat: add format option to cli

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import genDiff from './genDiff';
 const main = () => {
   program.version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
+    .option('-f, --format [type]', 'output format', 'object')
     .arguments('<firstConfig> <secondConfig>')
-    .action((firstConfig, secondConfig) => {
-      console.log(genDiff(firstConfig, secondConfig));
+    .action((firstConfig, secondConfig, cmdObj) => {
+      console.log(genDiff(firstConfig, secondConfig, cmdObj.format));
     });
   program.parse(process.argv);
 };

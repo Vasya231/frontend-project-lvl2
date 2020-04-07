a844855199134f0b4fc9cf964ffd275cd1180e53
Vasya231 <gg231589@gmail.com>
2020-04-07 13:23:52 +0300

    fix: make plain rendering not to display root name in path

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,18 +9,18 @@ const render = (diffObj) => {
       const { before: nodeBefore, after: nodeAfter } = node;
       if (!nodeBefore) {
         const addedValue = (_.isObject(nodeAfter.value) || _.has(nodeAfter, 'children')) ? '[complex value]' : `'${nodeAfter.value}'`;
-        return [`Property '${path}.${nodeAfter.name}' was added with value: ${addedValue}`];
+        return [`Property '${path}${nodeAfter.name}' was added with value: ${addedValue}`];
       }
       if (!nodeAfter) {
-        return [`Property '${path}.${nodeBefore.name}' was deleted`];
+        return [`Property '${path}${nodeBefore.name}' was deleted`];
       }
       const addedValue = (_.isObject(nodeAfter.value) || _.has(nodeAfter, 'children')) ? '[complex value]' : `'${nodeAfter.value}'`;
       const deletedValue = (_.isObject(nodeBefore.value) || _.has(nodeBefore, 'children')) ? '[complex value]' : `'${nodeBefore.value}'`;
-      return [`Property '${path}.${name}' was changed from ${deletedValue} to ${addedValue}`];
+      return [`Property '${path}${name}' was changed from ${deletedValue} to ${addedValue}`];
     }
     if (children) {
       return children
-        .map((child) => renderWithPath(path.concat(`.${name}`), child))
+        .map((child) => renderWithPath(name ? path.concat(`${name}.`) : path, child))
         .filter((child) => child)
         .join('\n');
     }

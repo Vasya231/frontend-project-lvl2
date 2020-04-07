52974c26bd3fbc199e9bfad9027f8a873ea0b0c9
Vasya231 <gg231589@gmail.com>
2020-04-07 14:42:35 +0300

    refactor: remove excessive nested function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,32 +1,30 @@
 import _ from 'lodash';
 
-const render = (diffObj) => {
-  const renderWithPath = (path, node) => {
-    const {
-      name, type, children,
-    } = node;
-    if (type === 'diff') {
-      const { before: nodeBefore, after: nodeAfter } = node;
-      if (!nodeBefore) {
-        const addedValue = (_.isObject(nodeAfter.value) || _.has(nodeAfter, 'children')) ? '[complex value]' : `'${nodeAfter.value}'`;
-        return [`Property '${path}${nodeAfter.name}' was added with value: ${addedValue}`];
-      }
-      if (!nodeAfter) {
-        return [`Property '${path}${nodeBefore.name}' was deleted`];
-      }
+
+const render = (node, path = '') => {
+  const {
+    name, type, children,
+  } = node;
+  if (type === 'diff') {
+    const { before: nodeBefore, after: nodeAfter } = node;
+    if (!nodeBefore) {
       const addedValue = (_.isObject(nodeAfter.value) || _.has(nodeAfter, 'children')) ? '[complex value]' : `'${nodeAfter.value}'`;
-      const deletedValue = (_.isObject(nodeBefore.value) || _.has(nodeBefore, 'children')) ? '[complex value]' : `'${nodeBefore.value}'`;
-      return [`Property '${path}${name}' was changed from ${deletedValue} to ${addedValue}`];
+      return [`Property '${path}${nodeAfter.name}' was added with value: ${addedValue}`];
     }
-    if (children) {
-      return children
-        .map((child) => renderWithPath(name ? path.concat(`${name}.`) : path, child))
-        .filter((child) => child)
-        .join('\n');
+    if (!nodeAfter) {
+      return [`Property '${path}${nodeBefore.name}' was deleted`];
     }
-    return '';
-  };
-  return renderWithPath('', diffObj);
+    const addedValue = (_.isObject(nodeAfter.value) || _.has(nodeAfter, 'children')) ? '[complex value]' : `'${nodeAfter.value}'`;
+    const deletedValue = (_.isObject(nodeBefore.value) || _.has(nodeBefore, 'children')) ? '[complex value]' : `'${nodeBefore.value}'`;
+    return [`Property '${path}${name}' was changed from ${deletedValue} to ${addedValue}`];
+  }
+  if (children) {
+    return children
+      .map((child) => render(child, name ? path.concat(`${name}.`) : path))
+      .filter((child) => child)
+      .join('\n');
+  }
+  return '';
 };
 
 export default render;

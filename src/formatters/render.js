3e49e7cf8a707356f6b1b78f48b8e5049d8c6d67
Vasya231 <gg231589@gmail.com>
2020-04-07 14:08:53 +0300

    feat: add json output format

--- src/formatters/render.js
+++ src/formatters/render.js
@@ -4,6 +4,7 @@ import objectRender from './object';
 const renders = {
   plain: plainRender,
   object: (diffObj) => objectRender(diffObj, -2),
+  json: (diffObj) => JSON.stringify(diffObj, null, 2),
 };
 
 export default (diffObj, format) => renders[format](diffObj);

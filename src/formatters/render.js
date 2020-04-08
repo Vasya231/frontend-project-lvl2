3079f5bf301541dae01b4f3bdccc88cab4b6270c
Vasya231 <gg231589@gmail.com>
2020-04-08 11:39:40 +0300

    refactor(render.js): replace table with switch

--- src/formatters/render.js
+++ src/formatters/render.js
@@ -1,10 +1,11 @@
 import plainRender from './plain';
 import objectRender from './object';
 
-const renders = {
-  plain: plainRender,
-  object: (diffObj) => objectRender(diffObj, -2),
-  json: (diffObj) => JSON.stringify(diffObj, null, 2),
+export default (diffObj, format) => {
+  switch (format) {
+    case 'plain': return plainRender(diffObj);
+    case 'object': return objectRender(diffObj, -2);
+    case 'json': return JSON.stringify(diffObj, null, 2);
+    default: throw new Error(`Wrong format: ${format}`);
+  }
 };
-
-export default (diffObj, format) => renders[format](diffObj);

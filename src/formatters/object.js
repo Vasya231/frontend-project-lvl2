7de57ffde6a6266d6bb7b9b5ef48be56b0ab1dc4
Vasya231 <gg231589@gmail.com>
2020-04-10 12:27:03 +0300

    refactor: remove superfluous functions

--- src/formatters/object.js
+++ src/formatters/object.js
@@ -16,36 +16,21 @@ const displayValue = (value, offset) => {
   return '{\n'.concat(movedProps, '\n', offsetStr, '}');
 };
 
-const renderSimpleNode = (node, currentPos, prefix) => {
-  const prefixStr = ' '.repeat(currentPos).concat(prefix, ' ');
-  // console.log(`rendering ${node.type} node with name ${node.name} and value ${node.value}`);
-  return `${prefixStr}${node.name}: ${displayValue(node.value, currentPos)}`;
-};
-
-const renderNodeAsAdded = (node, currentPos) => renderSimpleNode(node, currentPos, '+');
-
-const renderNodeAsRemoved = (node, currentPos) => renderSimpleNode(node, currentPos, '-');
-
-const renderEqualNode = (node, currentPos) => renderSimpleNode(node, currentPos, ' ');
-
-const renderDiffNode = (node, currentPos) => {
-  const added = renderNodeAsAdded({ ...node, value: node.added }, currentPos);
-  const removed = renderNodeAsRemoved({ ...node, value: node.removed }, currentPos);
-  return `${removed}${'\n'}${added}`;
-};
-
 const renderDualSourceNode = (diffObj, currentPos) => {
   const { name, children } = diffObj;
   const nestedPos = currentPos + offsetInc;
   const regularPrefix = ' '.repeat(currentPos > 0 ? currentPos : 0).concat('  ');
   const prefixForClosingQuote = ' '.repeat(currentPos + prefixLength);
+  const nestedPrefix = ' '.repeat(nestedPos);
   const childrenStr = children.reduce(
     (acc, child) => {
       switch (child.type) {
-        case 'added': return acc.concat('\n', renderNodeAsAdded(child, nestedPos));
-        case 'removed': return acc.concat('\n', renderNodeAsRemoved(child, nestedPos));
-        case 'changed': return acc.concat('\n', renderDiffNode(child, nestedPos));
-        case 'equal': return acc.concat('\n', renderEqualNode(child, nestedPos));
+        case 'added': return acc.concat('\n', `${nestedPrefix}+ ${child.name}: ${displayValue(child.value, nestedPos)}`);
+        case 'removed': return acc.concat('\n', `${nestedPrefix}- ${child.name}: ${displayValue(child.value, nestedPos)}`);
+        case 'changed': return acc
+          .concat('\n', `${nestedPrefix}- ${child.name}: ${displayValue(child.removed, nestedPos)}`)
+          .concat('\n', `${nestedPrefix}+ ${child.name}: ${displayValue(child.added, nestedPos)}`);
+        case 'equal': return acc.concat('\n', `${nestedPrefix}  ${child.name}: ${displayValue(child.value, nestedPos)}`);
         case 'dualSource': return acc.concat('\n', renderDualSourceNode(child, nestedPos));
         default: throw new Error(`Unexpected node type: ${child.type}`);
       }

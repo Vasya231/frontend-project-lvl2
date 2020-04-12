2bb658952fa865fc6bc4e10a601664bd12e68dc0
Vasya231 <gg231589@gmail.com>
2020-04-12 14:35:04 +0300

    refactor: remove code duplication in renderNodes

--- src/formatters/pretty.js
+++ src/formatters/pretty.js
@@ -3,40 +3,41 @@ import _ from 'lodash';
 const offsetInc = 4;
 const prefixLength = 2;
 
+const prefixes = {
+  added: '+ ',
+  removed: '- ',
+  unchanged: '  ',
+};
+
 const displayValue = (value, nestingDepth) => {
   if (!_.isPlainObject(value)) {
-    return value;
+    return value.toString();
   }
   const keys = Object.keys(value);
   const closingQuoteStr = `${' '.repeat(nestingDepth * offsetInc)}}`;
   const nestedOffsetStr = ' '.repeat((nestingDepth + 1) * offsetInc - prefixLength);
-  const props = keys.map((key) => `  ${key}: ${displayValue(value[key], nestingDepth + 1)}`);
+  const props = keys.map((key) => `${prefixes.unchanged}${key}: ${displayValue(value[key], nestingDepth + 1)}`);
   const movedProps = props.map((str) => `${nestedOffsetStr}${str}`).join('\n');
   return ['{', movedProps, closingQuoteStr].join('\n');
 };
 
-const renderNodes = (nodes, nestingDepth = 0) => {
+
+const renderNodes = (nodes, nestingDepth) => {
   const namesOffsetStr = ' '.repeat((nestingDepth + 1) * offsetInc - prefixLength);
   const closingQuoteStr = `${' '.repeat(nestingDepth * offsetInc)}}`;
   const renderedNodes = nodes.map((node) => {
     const { name, type } = node;
     switch (type) {
-      case 'added': {
-        const { value } = node;
-        return `${namesOffsetStr}+ ${name}: ${displayValue(value, nestingDepth + 1)}`;
-      }
-      case 'removed': {
-        const { value } = node;
-        return `${namesOffsetStr}- ${name}: ${displayValue(value, nestingDepth + 1)}`;
-      }
+      case 'added':
+      case 'removed':
       case 'unchanged': {
         const { value } = node;
-        return `${namesOffsetStr}  ${name}: ${displayValue(value, nestingDepth + 1)}`;
+        return `${namesOffsetStr}${prefixes[type]}${name}: ${displayValue(value, nestingDepth + 1)}`;
       }
       case 'changed': {
         const { valueBefore, valueAfter } = node;
-        const strForRemoved = `${namesOffsetStr}- ${name}: ${displayValue(valueBefore, nestingDepth + 1)}`;
-        const strForAdded = `${namesOffsetStr}+ ${name}: ${displayValue(valueAfter, nestingDepth + 1)}`;
+        const strForRemoved = `${namesOffsetStr}${prefixes.removed}${name}: ${displayValue(valueBefore, nestingDepth + 1)}`;
+        const strForAdded = `${namesOffsetStr}${prefixes.added}${name}: ${displayValue(valueAfter, nestingDepth + 1)}`;
         return `${strForRemoved}\n${strForAdded}`;
       }
       case 'complex': return `${namesOffsetStr}  ${name}: ${renderNodes(node.children, nestingDepth + 1)}`;

b192cc44d2b408f4b22d71ec72118ae79427a732
Vasya231 <gg231589@gmail.com>
2020-04-13 12:46:26 +0300

    test: combine input format tests with output format tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,11 +9,9 @@ const expectedEmpty = pathToFixture('resultEmpty.txt');
 
 const beforeYml = pathToFixture('before.yml');
 const afterYml = pathToFixture('after.yml');
-const expectedYml = pathToFixture('resultYml.txt');
 
 const beforeJson = pathToFixture('before.json');
 const afterJson = pathToFixture('after.json');
-const expectedJson = pathToFixture('resultJson.txt');
 
 const beforeIni = pathToFixture('before.ini');
 const afterIni = pathToFixture('after.ini');
@@ -21,19 +19,13 @@ const expectedIni = pathToFixture('resultIni.txt');
 
 const expectedPlainFormat = pathToFixture('resultPlain.txt');
 const expectedJsonFormat = pathToFixture('resultJsonFormat.txt');
+const expectedPrettyFormat = pathToFixture('resultPretty.txt');
 
 test.each([
-  ['test empty', beforeEmpty, afterEmpty, expectedEmpty],
-  ['test yml', beforeYml, afterYml, expectedYml],
-  ['test json', beforeJson, afterJson, expectedJson],
-  ['test ini', beforeIni, afterIni, expectedIni],
-])('%s', (testname, before, after, expected) => {
-  const expectedData = fs.readFileSync(expected, 'utf8');
-  expect(genDiff(before, after)).toEqual(expectedData);
-});
-
-test.each([
-  ['test plain formatting', beforeJson, afterJson, expectedPlainFormat, 'plain'],
+  ['test empty', beforeEmpty, afterEmpty, expectedEmpty, 'pretty'],
+  ['test yml, plain formatting', beforeYml, afterYml, expectedPlainFormat, 'plain'],
+  ['test json, pretty formatting', beforeJson, afterJson, expectedPrettyFormat, 'pretty'],
+  ['test ini', beforeIni, afterIni, expectedIni, 'pretty'],
   ['test json formatting', beforeJson, afterJson, expectedJsonFormat, 'json'],
 ])('%s', (testname, before, after, expected, format) => {
   const expectedData = fs.readFileSync(expected, 'utf8');

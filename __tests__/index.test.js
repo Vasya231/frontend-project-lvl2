3890ec406a637c7abc2fbcb256d9353994e08faa
Vasya231 <gg231589@gmail.com>
2020-04-16 17:01:18 +0300

    test: rewrite tests in one block

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,37 +15,18 @@ const afterIni = pathToFixture('after.ini');
 const expectedData = {};
 
 beforeAll(() => {
-  expectedData.plainFormat = fs.readFileSync(pathToFixture('resultPlain.txt'), 'utf8');
-  expectedData.jsonFormat = fs.readFileSync(pathToFixture('resultJsonFormat.txt'), 'utf8');
-  expectedData.prettyFormat = fs.readFileSync(pathToFixture('resultPretty.txt'), 'utf8');
+  expectedData.plain = fs.readFileSync(pathToFixture('resultPlain.txt'), 'utf8');
+  expectedData.json = fs.readFileSync(pathToFixture('resultJsonFormat.txt'), 'utf8');
+  expectedData.pretty = fs.readFileSync(pathToFixture('resultPretty.txt'), 'utf8');
 });
 
-describe('Test pretty formatting', () => {
-  test.each([
-    ['test yml', beforeYml, afterYml],
-    ['test json', beforeJson, afterJson],
-    ['test ini', beforeIni, afterIni],
-  ])('%s', (testname, before, after) => {
-    expect(genDiff(before, after, 'pretty')).toEqual(expectedData.prettyFormat);
-  });
-});
-
-describe('Test plain formatting', () => {
-  test.each([
-    ['test yml', beforeYml, afterYml],
-    ['test json', beforeJson, afterJson],
-    ['test ini', beforeIni, afterIni],
-  ])('%s', (testname, before, after) => {
-    expect(genDiff(before, after, 'plain')).toEqual(expectedData.plainFormat);
-  });
-});
-
-describe('Test json formatting', () => {
-  test.each([
-    ['test yml', beforeYml, afterYml],
-    ['test json', beforeJson, afterJson],
-    ['test ini', beforeIni, afterIni],
-  ])('%s', (testname, before, after) => {
-    expect(genDiff(before, after, 'json')).toEqual(expectedData.jsonFormat);
-  });
+test.each([
+  ['plain'],
+  ['pretty'],
+  ['json'],
+])('%s', (expectedFormat) => {
+  const expected = expectedData[expectedFormat];
+  expect(genDiff(beforeJson, afterJson, expectedFormat)).toEqual(expected);
+  expect(genDiff(beforeYml, afterYml, expectedFormat)).toEqual(expected);
+  expect(genDiff(beforeIni, afterIni, expectedFormat)).toEqual(expected);
 });
